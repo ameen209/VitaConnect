a1f44690271c842682b4b5c7b427489168619855
ameen209 <ameenvit22@gmail.com>
2025-08-29 00:29:08 +0530

    Navbar in progress

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import {assets} from '../assets/assets'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>
-      
+    <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
+      <img className='w-44 cursor-pointer' src={assets.logo} alt="" />
+      <ul className='hidden md:flex items-start gap-5 font-medium'>
+        <NavLink>
+          <li className='py-1'>HOME</li>
+          <hr style={{backgroundColor: "#5f6FFF"}} className='border-none outline-none h-0.5 w-3/5 m-auto ' />
+        </NavLink>
+        <NavLink>
+          <li className='py-1'>DOCTORS</li>
+          <hr style={{backgroundColor: "#5f6FFF"}} className='border-none outline-none h-0.5 w-3/5 m-auto ' />
+        </NavLink>
+        <NavLink>
+          <li className='py-1'>ABOUT</li>
+          <hr style={{backgroundColor: "#5f6FFF"}} className='border-none outline-none h-0.5 w-3/5 m-auto ' />
+        </NavLink>
+        <NavLink>
+          <li className='py-1'>CONTACT</li>
+          <hr style={{backgroundColor: "#5f6FFF"}} className='border-none outline-none h-0.5 w-3/5 m-auto ' />
+        </NavLink>
+      </ul>
+      <div>
+        <button>Create Account</button>
+      </div>
     </div>
   )
 }
