1460d434ea9ccd698d8185237b925e8b278c65e1
ameen209 <ameenvit22@gmail.com>
2026-01-03 19:38:52 +0530

    Admin Panel fully created

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,7 @@ app.use(express.json())
 app.use(cors({
     origin : [
         "http://localhost:5173",
+        "http://localhost:5174", 
         "https://vita-connect.vercel.app/"
     ],
     credentials: true
